cd35152af03a35b2be8df89080a1e3a5f0ede51a
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-06-22 11:51:08 -0700

    Added css autoprefix

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp')
+  , filter = require('gulp-filter')
+  , less = require('gulp-less')
+  , prefix = require('gulp-autoprefixer')
+  , gfm = require('gulp-front-matter')
+  , gulpsmith = require('gulpsmith')
+  , collections = require('metalsmith-collections')
+  , markdown = require('metalsmith-markdown')
+  , templates = require('metalsmith-templates')
+  , permalinks = require('metalsmith-permalinks')
+  , assign = require('lodash.assign')
+  , rimraf = require('rimraf')
+  , express = require('express')
+
+gulp.task('clean', function(cb){
+  rimraf('build/(!(css))', cb);
+});
+
+gulp.task('build', ['clean'], function() {
+
+  gulp.src(['src/content/**/*.md'])
+    .pipe(gfm()).on('data', function(file) {
+      assign(file, file.frontMatter); 
+      delete file.frontMatter;
+    })
+    .pipe(
+      gulpsmith(__dirname)
+        .use(collections({
+          projects:{}
+        }))
+        .use(markdown({
+          smartypants: true
+        }))
+        .use(templates({
+          engine: 'jade',
+          directory: 'src/templates'
+        }))
+        .use(permalinks({
+          pattern: ':permalink'
+        }))
+    )
+    .pipe(gulp.dest('build'));
+
+});
+
+gulp.task('less', function() {
+  gulp.src(['src/less/style.less'])
+    .pipe(less())
+    .pipe(prefix('last 2 versions'))
+    .pipe(gulp.dest('build/css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch(['src/content/**/*', 'src/templates/**/*'], ['build'])
+  gulp.watch('src/less/**/*.less', ['less'])
+});
+
+gulp.task('server', function() {
+  var server = express();
+  server.use(express.static('build'));
+  server.listen(1337);
+});
+
+gulp.task('default', ['build', 'less', 'server', 'watch'])
