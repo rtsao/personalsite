b4754b104415117d0be99c1e5e387a81025c9c5d
Ryan Tsao <ryan.j.tsao@gmail.com>
2014-06-25 19:58:00 -0700

    Updated gulpfile, added imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp')
   , less = require('gulp-less')
   , prefix = require('gulp-autoprefixer')
   , gfm = require('gulp-front-matter')
+  , imagemin = require('gulp-imagemin')
+  , cache = require('gulp-cache')
   , gulpsmith = require('gulpsmith')
   , collections = require('metalsmith-collections')
   , markdown = require('metalsmith-markdown')
@@ -14,10 +16,10 @@ var gulp = require('gulp')
   , express = require('express')
 
 gulp.task('clean', function(cb) {
-  del(['build/**/*','!build/css/**'], cb);
+  del(['build/**/*'], cb);
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('build', function() {
 
   gulp.src(['src/content/**/*.md'])
     .pipe(gfm()).on('data', function(file) {
@@ -27,20 +29,19 @@ gulp.task('build', ['clean'], function() {
     .pipe(
       gulpsmith(__dirname)
         .use(collections({
-          projects:{reverse: true}
+          projects: {reverse: true}
         }))
         .use(markdown({
           smartypants: true
+        }))
+        .use(permalinks({
+          pattern: ':collection/:slug'
         }))
         .use(excerpts())
         .use(templates({
           engine: 'jade',
           directory: 'src/templates'
         }))
-        .use(permalinks({
-          pattern: ':collection/:slug'
-        }))
-
     )
     .pipe(gulp.dest('build'));
 
@@ -53,9 +54,17 @@ gulp.task('less', function() {
     .pipe(gulp.dest('build/css'))
 })
 
+gulp.task('images', function(cb) {
+  del(['build/img/**/*'], cb);
+  gulp.src(['src/images/**/*'])
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('build/img'))
+});
+
 gulp.task('watch', function() {
   gulp.watch(['src/content/**/*', 'src/templates/**/*'], ['build'])
   gulp.watch('src/less/**/*.less', ['less'])
+  gulp.watch('src/images/**/*', ['images'])
 });
 
 gulp.task('server', function() {
@@ -64,4 +73,4 @@ gulp.task('server', function() {
   server.listen(1337);
 });
 
-gulp.task('default', ['build', 'less', 'server', 'watch'])
+gulp.task('default', ['build', 'images', 'less', 'server', 'watch'])
